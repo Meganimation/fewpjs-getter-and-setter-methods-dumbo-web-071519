20b4594e31417c6b307dd8a985a9a5a6153424da
Megan Adams <meganoliviawendyadams@gmail.com>
2019-09-06 00:59:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ class Circle {
     return this.radius * 2
   }
   get circumference() {
-    return (Math.PI * diameter)
+    return Math.PI * diameter
   }
   get area()
     { return Math.PI * (this.radius * this.radius)
